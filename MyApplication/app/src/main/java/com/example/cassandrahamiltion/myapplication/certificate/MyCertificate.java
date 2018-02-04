4f7b8a65045bb8066b5dc3284e539482ecd279f7
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2018-02-04 13:22:44 -0800

    Resolved MyScript gradle issues

---
+++ MyApplication/app/src/main/java/com/example/cassandrahamiltion/myapplication/certificate/MyCertificate.java
@@ -0,0 +1,11 @@
+// Copyright MyScript. All rights reserved.
+
+package com.example.cassandrahamiltion.myapplication.certificate;
+
+public final class MyCertificate
+{
+  public static final byte[] getBytes()
+  {
+    throw new RuntimeException("Please replace the content of MyCertificate.java with the certificate you received from the developer portal");
+  }
+}
